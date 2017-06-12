404c1909b6880c96357d452ab3eff123496b28ad
Braulio De Leon <bradelev@hotmail.com>
2017-06-12 20:28:49 -0300

    changes on layout

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,31 @@
-$(document).ready(init);
+$(init);
 
 function transition(element) {
   $('.js-nav li').click(function() {
     $('html, body').animate({
-        scrollTop: $('#publicaciones').offset().top
+        scrollTop: $('#noticias').offset().top
     }, 1000);
   });
 }
 
+function waypoints() {
+  var waypoint = new Waypoint({
+  element: document.getElementById('noticias'),
+  handler: function(direction) {
+    if (direction === 'down') {
+
+      console.log('Basic waypoint triggered, down')
+    } else {
+      console.log('Basic waypoint triggered, up')
+
+    }
+    
+  }
+})
+}
 
 function init() {
-  transition();
+  // alert('carga');
+  // transition();
+  waypoints()
 }
