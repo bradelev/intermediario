8d53dff7d40679f34e126e695f0f22634231c05a
Braulio De Leon <bradelev@hotmail.com>
2017-06-13 01:36:30 -0300

    estructura armada

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,30 +2,15 @@ $(init);
 
 function transition(element) {
   $('.js-nav li').click(function() {
+    console.log('emras13')
     $('html, body').animate({
         scrollTop: $('#noticias').offset().top
     }, 1000);
   });
 }
 
-function waypoints() {
-  var waypoint = new Waypoint({
-  element: document.getElementById('noticias'),
-  handler: function(direction) {
-    if (direction === 'down') {
-
-      console.log('Basic waypoint triggered, down')
-    } else {
-      console.log('Basic waypoint triggered, up')
-
-    }
-    
-  }
-})
-}
 
 function init() {
   // alert('carga');
-  // transition();
-  waypoints()
+  transition();
 }
